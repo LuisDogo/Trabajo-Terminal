749069090b085d9375fb867688b2b176fec17db4
Luis Daniel Moreno S. <luisdogoms@gmail.com>
2025-05-15 19:49:12 -0600

    desastre ahora, limpieza en el siguiente commit

--- NN_dataset_generator.py
+++ NN_dataset_generator.py
@@ -3,16 +3,20 @@ import pandas as pd
 import os, glob
 
 def main(period):
+    n_rows = 200
     people = []
     path = "Data/Preprocessed/"
     
     for filename in glob.glob(os.path.join(path, '*.csv')):
         df = pd.read_csv(filename, sep = ";")
-        people.append(df.head(201))
+        people.append(df.head(n_rows + period))
 
     df = pd.concat(people)
+    print(df.loc[199,:])
     df[f"glucose_t+{period}"] = df["glucose"].shift(periods = -period)
-    df = df.drop(index = 200)
+    df = df.drop(index = n_rows + period - 1)
+    print(df.loc[199,:])
+
     #! por el momento el datetime causa problemas con pytorch, así que lo ignoré, pero pienso reintroducirlo
     #! porque la correlación de time es de .1 > a las demás variables
     # t = df["time"].apply(lambda time: np.datetime64(time))
@@ -26,5 +30,5 @@ def main(period):
 
 
 if __name__ == "__main__":
-    period = 3
+    period = 1
     main(period)
